b3c6dedf860ca5b66554b6f47e29d38dfa4334a9
CrisAriasQ <163191457+CrisAriasQ@users.noreply.github.com>
2025-07-10 12:27:48 -0400

    Item 1 - part 1
    Integrantes

---
+++ integrantes.py
@@ -0,0 +1,7 @@
+integrantes = ["Cristián Arias", "Mayra Marin "]
+ruts = ["C= 20.963.459-7", "M= 20.909.226-3"]
+print("Integrantes del grupo:")
+for i in integrantes:
+    print(i)
+for r in ruts:
+    print(r)
